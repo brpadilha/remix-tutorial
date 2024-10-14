2031cced8aaa4aa4d16a72ac2b6f0847fbcebea7
Bruno Padilha <brpadilha.dev@gmail.com>
2024-10-14 15:08:24 -0300

    Refactor navigation links in App component

--- app/routes/contacts.$contactId_.edit.tsx
+++ app/routes/contacts.$contactId_.edit.tsx
@@ -1,5 +1,5 @@
 import { json, LoaderFunctionArgs } from "@remix-run/node";
-import { Form, redirect, useLoaderData } from "@remix-run/react";
+import { Form, redirect, useLoaderData, useNavigate } from "@remix-run/react";
 import invariant from "tiny-invariant";
 import { getContact, updateContact } from "~/data";
 
@@ -27,7 +27,7 @@ export const action = async ({params, request}: LoaderFunctionArgs) => {
 export default function EditContact() {
 
   const { contact } = useLoaderData<typeof loader>();
-
+  const navigate = useNavigate()
   return(
     <Form key={contact.id} id="contact-form" method="POST">
       <p>
@@ -76,7 +76,11 @@ export default function EditContact() {
       </label>
       <p>
         <button type="submit">Save</button>
-        <button type="button">Cancel</button>
+        <button type="button"
+          onClick={() => {
+            navigate(-1)
+          }}
+        >Cancel</button>
       </p>
     </Form>
   );
